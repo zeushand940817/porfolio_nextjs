6fdd43f6c8467a5a2f888f053e5a773b77313c18
zeushand940817 <zeushand940817@gmail.com>
2019-01-23 14:22:48 -0800

    working on route masking

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
 import Layout from "../components/MyLayout.js";
+import Link from "next/link";
 
-const Index = () => (
-  <Layout>
-    <p>Hello Next.js</p>
-  </Layout>
+const PostLink = props => (
+  <li>
+    <Link as={`/p/${props.id}`} href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
 );
 
-export default Index;
+export default () => (
+  <Layout>
+    <h1>My Blog</h1>
+    <ul>
+      <PostLink id="hello-nextjs" title="Hello Next.js" />
+      <PostLink id="learn-nextjs" title="Learn Next.js is awesome" />
+      <PostLink id="deploy-nextjs" title="Deploy apps with Zeit" />
+    </ul>
+  </Layout>
+);
