9dc3511d2b2116608fe93db8ca23bf81f4da8b0c
zeushand940817 <zeushand940817@gmail.com>
2019-01-24 10:45:53 -0800

    getting font sizing how I want it

--- components/Menu/Menu.js
+++ components/Menu/Menu.js
@@ -7,29 +7,45 @@ const linkStyle = {
 
 const Menu = () => (
   <div className="menu">
-    <Link href="/">
-      <a className="header" style={linkStyle}>
-        Work
-      </a>
-    </Link>
-    <Link href="/about">
-      <a style={linkStyle}>Films</a>
-    </Link>
-    <Link href="/about">
-      <a style={linkStyle}>Stills</a>
-    </Link>
-    <Link href="/about">
-      <a style={linkStyle}>Code</a>
-    </Link>
-    <Link href="/about">
-      <a style={linkStyle}>3D</a>
-    </Link>
-    <Link href="/about">
-      <a style={linkStyle}>Thoughts</a>
-    </Link>
-    <Link href="/about">
-      <a style={linkStyle}>Contact</a>
-    </Link>
+    <ul>
+      <li>
+        <Link href="/">
+          <a className="header" style={linkStyle}>
+            Work
+          </a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/about">
+          <a style={linkStyle}>Films</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/about">
+          <a style={linkStyle}>Stills</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/about">
+          <a style={linkStyle}>Code</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/about">
+          <a style={linkStyle}>3D</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/about">
+          <a style={linkStyle}>Thoughts</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/about">
+          <a style={linkStyle}>Contact</a>
+        </Link>
+      </li>
+    </ul>
   </div>
 );
 
