6b61ef551c5799b26761b9f8a16386c8932c3d0f
zeushand940817 <zeushand940817@gmail.com>
2019-01-24 09:28:13 -0800

    adding in the header content I want

--- components/header.js
+++ components/header.js
@@ -7,10 +7,25 @@ const linkStyle = {
 const Header = () => (
   <div>
     <Link href="/">
-      <a style={linkStyle}>Home</a>
+      <a style={linkStyle}>Work</a>
     </Link>
     <Link href="/about">
-      <a style={linkStyle}>About</a>
+      <a style={linkStyle}>Films</a>
+    </Link>
+    <Link href="/about">
+      <a style={linkStyle}>Stills</a>
+    </Link>
+    <Link href="/about">
+      <a style={linkStyle}>Code</a>
+    </Link>
+    <Link href="/about">
+      <a style={linkStyle}>3D</a>
+    </Link>
+    <Link href="/about">
+      <a style={linkStyle}>Thoughts</a>
+    </Link>
+    <Link href="/about">
+      <a style={linkStyle}>Contact</a>
     </Link>
   </div>
 );
