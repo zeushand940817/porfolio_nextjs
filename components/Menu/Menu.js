c35ba58e019b367dbd2d233bad788fd4662831cf
zeushand940817 <zeushand940817@gmail.com>
2019-01-24 11:07:46 -0800

    getting a POC of the strikthrough done

--- components/Menu/Menu.js
+++ components/Menu/Menu.js
@@ -11,7 +11,7 @@ const Menu = () => (
       <li>
         <Link href="/">
           <a className="header" style={linkStyle}>
-            Work
+            <span>Work</span>
           </a>
         </Link>
       </li>
