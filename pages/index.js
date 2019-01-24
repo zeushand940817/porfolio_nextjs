312db3f9eac96d29e559cc301a5822fac7a0c966
zeushand940817 <zeushand940817@gmail.com>
2019-01-24 15:41:20 -0800

    adding some base content

--- pages/index.js
+++ pages/index.js
@@ -4,81 +4,58 @@ import Head from "next/head";
 
 import "../styles.scss";
 
-function getPosts() {
-  return [
-    { id: "hello-nextjs", title: "Hello Next.js" },
-    { id: "learn-nextjs", title: "Learn Next.js is awesome" },
-    { id: "deploy-nextjs", title: "Deploy apps with ZEIT" }
-  ];
-}
+const HeadContent = () => (
+  <Head>
+    <title>JP Kubala</title>
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+      integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
+      crossorigin="anonymous"
+    />
+  </Head>
+);
 
-const PostLink = ({ post }) => (
-  <li>
-    <Link as={`/p/${post.id}`} href={`/post?title=${post.title}`}>
-      <a className="example">{post.title}</a>
-    </Link>
-    <style jsx>{`
-      li {
-        list-style: none;
-        margin: 5px 0;
-      }
-
-      a {
-        text-decoration: none;
-        color: blue;
-        font-family: "Arial";
-      }
-
-      a:hover {
-        opacity: 0.6;
-      }
-    `}</style>
-  </li>
+const Brick = () => (
+  <div className="wall">
+    <div className="brick">
+      <div class="pixels-photo">
+        <p>
+          <img
+            src="https://drscdn.500px.org/photo/228036047/m%3D900/v2?user_id=9643357&webp=true&sig=52dc2f0241948f9b16917788e9101d9a44187aac4716397e0662d1c7aac2e7b2"
+            alt="Backside Sand Dunes by Paul Kubala on 500px.com"
+          />
+        </p>
+        <a
+          href="https://500px.com/photo/228036047/backside-sand-dunes-by-paul-kubala"
+          alt="Backside Sand Dunes by Paul Kubala on 500px.com"
+        />
+      </div>
+      <div className="caption">Southwest Colorado Photojournalism</div>
+    </div>
+    <div className="brick">
+      <div class="pixels-photo">
+        <p>
+          <img
+            src="https://drscdn.500px.org/photo/228036047/m%3D900/v2?user_id=9643357&webp=true&sig=52dc2f0241948f9b16917788e9101d9a44187aac4716397e0662d1c7aac2e7b2"
+            alt="Backside Sand Dunes by Paul Kubala on 500px.com"
+          />
+        </p>
+        <a
+          href="https://500px.com/photo/228036047/backside-sand-dunes-by-paul-kubala"
+          alt="Backside Sand Dunes by Paul Kubala on 500px.com"
+        />
+      </div>
+      <div className="caption">Iceland Photojournalism</div>
+    </div>
+  </div>
 );
 
 export default () => (
   <div>
-    <Head>
-      <title>JP Kubala</title>
-      <link
-        rel="stylesheet"
-        href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
-        integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
-        crossorigin="anonymous"
-      />
-    </Head>
-
+    <HeadContent />
     <Layout>
-      <h1>My Blog</h1>
-      <ul>
-        {getPosts().map(post => (
-          <PostLink key={post.id} post={post} />
-        ))}
-      </ul>
-      <style jsx>{`
-        h1,
-        a {
-          font-family: "Arial";
-        }
-
-        ul {
-          padding: 0;
-        }
-
-        li {
-          list-style: none;
-          margin: 5px 0;
-        }
-
-        a {
-          text-decoration: none;
-          color: blue;
-        }
-
-        a:hover {
-          opacity: 0.6;
-        }
-      `}</style>
+      <Brick />
     </Layout>
   </div>
 );
