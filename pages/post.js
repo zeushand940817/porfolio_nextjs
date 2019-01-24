478385c22abc0b5e261ed8f865d9ece1a1bcb839
zeushand940817 <zeushand940817@gmail.com>
2019-01-24 13:02:12 -0800

    cleaning up posts

--- pages/post.js
+++ pages/post.js
@@ -2,4 +2,4 @@ import Layout from "../components/Layout/Layout.js";
 import { withRouter } from "next/router";
 import Markdown from "react-markdown";
 
-export default withRouter(props => <Layout>Herm</Layout>);
+export default withRouter(props => <Layout>Perst</Layout>);
