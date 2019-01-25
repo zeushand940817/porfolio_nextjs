e8daf2b6b9cc2dfafa8ab3acb5a1245aaeaa1667
zeushand940817 <zeushand940817@gmail.com>
2019-01-24 19:01:47 -0800

    adding one solution to the form

--- pages/contact.js
+++ pages/contact.js
@@ -2,6 +2,17 @@ import Layout from "../components/Layout/Layout.js";
 
 export default () => (
   <Layout>
-    <p>This is the contact Page</p>
+    <h2>Say Hello.</h2>
+    <p>
+      Whether its about potential work or you just want to geek out about
+      movies, I love to talk.
+    </p>
+    <form action="https://formspree.io/jp@jpkubala.com" method="POST">
+      <label>Name</label>
+      <input type="text" name="name" />
+      <input type="text" name="name" />
+      <textarea type="text" name="name" />
+      <input type="submit" value="Submit" />
+    </form>
   </Layout>
 );
