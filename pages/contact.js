fcf8cf7a1d06c4dfa9c10a7572303659132f401b
zeushand940817 <zeushand940817@gmail.com>
2019-02-14 15:06:33 -0800

    adding some more progress to the about page

--- pages/contact.js
+++ pages/contact.js
@@ -14,35 +14,31 @@ export default () => (
     </Head>
 
     <Layout>
-      <div className="contact">
-        <h2>Say Hello.</h2>
-        <div className="row">
-          <div className="col col-25">
-            <p>
-              Whether its about potential work or you just want to geek out
-              about movies, I love to talk. Feel free to contact me on my form
-              or send me an email any time.
-            </p>
-            <p>
-              <i class="fas fa-mobile-alt" /> 303-919-5398 <br />
-              <i class="far fa-envelope" /> jp@jpkubala.
-              <br />
-              <i class="fas fa-map-marked-alt" /> Portland, OR. 97210 <br />
-            </p>
-          </div>
-          <div className="col">
-            <form action="https://formspree.io/jp@jpkubala.com" method="POST">
-              <input
-                type="text"
-                autofocus="autofocus"
-                placeholder="Name"
-                name="name"
-              />
-              <input type="text" name="name" placeholder="Email" />
-              <textarea type="text" name="name" placeholder="Message" />
-              <input type="submit" value="Submit" />
-            </form>
-          </div>
+      <h2>Say Hello.</h2>
+      <div className="content contact">
+        <div className="gutter-100">
+          <form action="https://formspree.io/jp@jpkubala.com" method="POST">
+            <input
+              type="text"
+              autofocus="autofocus"
+              placeholder="Name"
+              name="name"
+            />
+            <input type="text" name="name" placeholder="Email" />
+            <textarea type="text" name="name" placeholder="Message" />
+            <input type="submit" value="Submit" />
+          </form>
+          <p>
+            Whether its about potential work or you just want to geek out about
+            movies, I love to talk. Feel free to contact me on my form or send
+            me an email any time.
+          </p>
+          <p>
+            303-919-5398 <br />
+            jp@jpkubala.com
+            <br />
+            Portland, OR. 97210 <br />
+          </p>
         </div>
       </div>
     </Layout>
