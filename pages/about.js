ef8fc6f14e2f38611a8f8971ce31d6285231737c
zeushand940817 <zeushand940817@gmail.com>
2019-02-14 11:56:54 -0800

    almost have about page layed out

--- pages/about.js
+++ pages/about.js
@@ -2,21 +2,23 @@ import Layout from "../components/Layout/Layout.js";
 
 export default () => (
   <Layout>
-    <img className="me" src="/static/hero.jpeg" />
-    <img className="me" src="/static/hero2.jpeg" />
-    <img
-      src="https://drscdn.500px.org/photo/222584935/m%3D900/v2?user_id=9643357&webp=true&sig=10fec4f1e4f41a6884f75d9d854d2fb9a183a729844a9971257ddec157be8fb5"
-      alt="Standing in Awe, Bierstadt by Paul Kubala on 500px.com"
-    />
-    <p>
-      Paul Kubala is a creative technolghist based out of Portland, Oregon who
-      loves to tell stories through different mediums. From photography, to
-      animation, to filmmaking, to creating interactive AR applications he loves
-      to push the boundries of art mixed with tehcnolghy.
-    </p>
-    <p>
-      When he is not working, you can catch him in the outdoors whitewater
-      kayaking, skiing, working on his camper van, or eating great tacos.
-    </p>
+    <h2>ABOUT</h2>
+    <div className="content about">
+      <img
+        src="https://drscdn.500px.org/photo/222584935/m%3D900/v2?user_id=9643357&webp=true&sig=10fec4f1e4f41a6884f75d9d854d2fb9a183a729844a9971257ddec157be8fb5"
+        alt="Standing in Awe, Bierstadt by Paul Kubala on 500px.com"
+        className="me"
+      />
+      <p>
+        Paul Kubala is a creative technolghist based out of Portland, Oregon who
+        loves to tell stories through different mediums. From photography, to
+        animation, to filmmaking, to creating interactive AR applications he
+        loves to push the boundries of art mixed with tehcnolghy.
+      </p>
+      <p>
+        When he is not working, you can catch him in the outdoors whitewater
+        kayaking, skiing, working on his camper van, or eating great tacos.
+      </p>
+    </div>
   </Layout>
 );
