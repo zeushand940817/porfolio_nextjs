5027bcd3f4f5b5ab489332b55ed37d2d01ce0285
zeushand940817 <zeushand940817@gmail.com>
2019-02-14 10:52:42 -0800

    adding the 3 grid system

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,13 @@ export default () => (
         <div className="gutter">
           <Brick
             title="The Oblong Adventure"
-            source="/static/the_oblong_adventure/hero.png"
+            source="/static/the_oblong_adventure/detail.png"
             link="/oblongadventure"
+          />
+          <Brick
+            title="openGL Pacman P.O.C."
+            source="/static/pacman/pacman.gif"
+            link="/pacman"
           />
         </div>
         <div className="gutter">
@@ -20,6 +25,11 @@ export default () => (
             source="/static/tic_tac_toe/hero.png"
             link="/tictactoe"
           />
+          <Brick
+            title="The Mess 16mm Film"
+            source="/static/the_mess/theMess.gif"
+            link="/themess"
+          />
         </div>
         <div className="gutter">
           <Brick
@@ -27,88 +37,16 @@ export default () => (
             source="/static/sw-colorado/IMG_9838.png"
             link=""
           />
+          <Brick
+            title="I Remember 16mm Film"
+            source="/static/i_remember/Iremember.gif"
+            link="/iremember"
+          />
         </div>
       </div>
     </Layout>
   </div>
 );
 
-// test = "  <div
-//     style={{
-//       width: "60%",
-//       height: "370px",
-//       margin: "10px"
-//     }}
-//   >
-//     <Brick
-//       title="The Oblong Adventure"
-//       source="/static/the_oblong_adventure/hero.png"
-//       link="/oblongadventure"
-//     />
-//   </div>
-//   <div
-//     style={{
-//       width: "30%",
-//       height: "360px",
-//       margin: "10px"
-//     }}
-//   >
-//     <Brick
-//       title="Tic Tac Toe Game"
-//       source="/static/tic_tac_toe/hero.png"
-//       link="/tictactoe"
-//     />
-//   </div>
-//   <div
-//     style={{
-//       width: "60%",
-//       height: "500px",
-//       margin: "10px"
-//     }}
-//   >
-//     <Brick
-//       title="Southwest Colorado Photojournalism"
-//       source="/static/sw-colorado/IMG_9838.png"
-//       link=""
-//     />
-//   </div>
-//   <div
-//     style={{
-//       width: "30%",
-//       height: "240px",
-//       margin: "10px"
-//     }}
-//   >
-//     <Brick
-//       title="openGL Pacman P.O.C."
-//       source="/static/pacman/pacman.gif"
-//       link="/pacman"
-//     />
-//   </div>
-//   <div
-//     style={{
-//       width: "45%",
-//       height: "400px",
-//       margin: "10px"
-//     }}
-//   >
-//     <Brick
-//       title="The Mess 16mm Film"
-//       source="/static/the_mess/theMess.gif"
-//       link="/themess"
-//     />
-//   </div>
-//   <div
-//     style={{
-//       width: "45%",
-//       height: "340px",
-//       margin: "10px"
-//     }}
-//   >
-//     <Brick
-//       title="I Remember 16mm Film"
-//       source="/static/i_remember/Iremember.gif"
-//       link="/iremember"
-//     />
 //   </div>
 // </div>"
