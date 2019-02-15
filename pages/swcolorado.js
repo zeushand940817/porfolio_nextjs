7a019518e36c7ca11504bdbd538e406967445fab
zeushand940817 <zeushand940817@gmail.com>
2019-02-15 09:10:40 -0800

    adding strucuture for sw-colorado

--- pages/swcolorado.js
+++ pages/swcolorado.js
@@ -2,15 +2,20 @@ import Layout from "../components/Layout/Layout.js";
 
 export default () => (
   <Layout>
-    <h2>Southwest Colorado Photojournalism</h2>
-    <p>Photography. Photojournalism</p>
-    <p>
-      In 2016 my friend Jacob and I embarked on a 3 day Adventure through some
-      of the less known parts of southwestern colorado. The whole story is
-      published on my blog.
-    </p>
-    <ul>
-      <li>Story: coming soon!</li>
-    </ul>
+    <div className="content photo">
+      <h2>Southwest Colorado Photojournalism</h2>
+      <div className="gutter-50">
+        <img src="/static/sw-colorado/IMG_9838.png" />
+      </div>
+      <div className="gutter-50">
+        <p>Photography. Photojournalism</p>
+        <p>
+          In 2016 my friend Jacob and I embarked on a 3 day Adventure through
+          some of the less known parts of southwestern colorado. The adventure
+          included hiking a 14,000ft peak, going to a national park, and
+          aligator wrestling.
+        </p>
+      </div>
+    </div>
   </Layout>
 );
